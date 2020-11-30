40ba6e19a2e04a56529f227ac0085fd11597330f
meobanak113 meobanak113 <meobanak8@gmail.com>
2020-12-01 00:28:44 +0700

    some change

--- DLLToScript/DLLToScript.cs
+++ DLLToScript/DLLToScript.cs
@@ -17,25 +17,36 @@ namespace DLLToScript
 
         public void GetFiles()
         {
+            string result = "";
             //var curDir = Directory.GetCurrentDirectory();
-            string curDir = @"C:\Users\nguyen tam\Desktop\Github\test";
+            string curDir = @"D:\test";
 
             string[] myFiles = Directory.GetFiles(curDir);
             foreach (string myFile in myFiles)
             {
-                string strbase64 = myFile.EncodeFileToStringBase64();
-                AssemblyModel info = myFile.GetInfoDLL();
-                //info.DATA = strbase64;
-
-                var result = ToQuery(info);
-
-                Console.WriteLine(myFile);
+                if(Path.GetExtension(myFile) == ".dll")
+                {
+                    string strbase64 = myFile.EncodeFileToStringBase64();
+                    AssemblyModel info = myFile.GetInfoDLL();
+                    info.DATA = strbase64;
+                    result += ToQuery(info) + "\n" + "\n";
+                    Console.WriteLine(myFile + " Ok");
+                }
             }
+
+            if (String.IsNullOrEmpty(result))
+                return;
+
+            var path = Path.Combine(curDir, "Script_" + DateTime.Now.ToString("yyyyMMdd_HHMMss") + ".sql");
+            byte[] data = Encoding.UTF8.GetBytes(result);
+
+            File.WriteAllBytes(path, data);
+            Console.ReadLine();
         }
 
         private string ToQuery(AssemblyModel model)
         {
-            string result = "IF EXISTS(SELECT 1 FROM Application..L_Applications(NOLOCK) WHERE OriginalFileName = '" + model.OriginalFileName + "')"
+            string result = "IF NOT EXISTS(SELECT 1 FROM Application..L_Applications(NOLOCK) WHERE OriginalFileName = '" + model.OriginalFileName + "')"
                 +"\n" + "BEGIN" + "\n" +
                 InsertQuery(model)
                 + "\n" + "END" + "\n" +
@@ -53,8 +64,8 @@ namespace DLLToScript
             "(ProductName,OriginalFileName,Version,DATA,CreatedDate, ModifiedDate)" + "\n" +
             " VALUES(" + "'" + model.ProductName + "', '"
            +  model.OriginalFileName + "', '" + model.Version + "'," + "\n" +
-            "N'" + model.DATA + "', " + "\n" +
-           model.CreatedDate +", " + model.CreatedDate + ")";
+            "'" + model.DATA + "', " + "\n" +
+            "'" + model.CreatedDate +"', '" + model.CreatedDate + "')";
             return result;
         }
 
@@ -62,18 +73,14 @@ namespace DLLToScript
         {
             string result = "UPDATE Application..L_Applications " + "\n" +
             "SET Version = '" + model.Version + "'," + "\n" +
-            "DATA = N'" + model.DATA + "'" + "\n" +
-            "WHERE OriginalFileName = '" + model.OriginalFileName;
+            "DATA = '" + model.DATA + "'" + "\n" +
+            "WHERE OriginalFileName = '" + model.OriginalFileName + "'";
             return result;
         }
 
         public void ExportToSqlFile()
         {
-            string result = "USE APPLICATION ";
-            result += "GO";
-
-
-
+           
         }
     }
 }
